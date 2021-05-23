a5dd4648e5ae36acf24dbef71b72905a89a6aa91
David <myemail@none.com>
2021-05-23 16:34:34 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,20 @@ Adafruit Microphone Amplifier
 #include <FastLED.h>
 #define PIN 6
 #define N_PIXELS  27
-#define BG 0
-#define COLOR_ORDER GRB  // Try mixing up the letters (RGB, GBR, BRG, etc) for a whole new world of color combinations
 #define BRIGHTNESS 255   // 0-255, higher number is brighter.
 #define LED_TYPE WS2812B
 #define MIC_PIN   A4  // Microphone is attached to this analog pin
-#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
-#define NOISE     10  // Noise/hum/interference in mic signal
-#define SAMPLES   60  // Length of buffer for dynamic level adjustment
 #define TOP       (N_PIXELS + 2) // Allow dot to go slightly off scale
-#define PEAK_FALL 20  // Rate of peak falling dot
 #define N_PIXELS_HALF (N_PIXELS/2)
-#define GRAVITY   -9.81 // Downward (negative) acceleration of gravity in m/s^2
-#define h0        0.5   // Starting height, in meters, of the ball (strip length)
-#define NUM_BALLS 3     // Number of bouncing balls you want (recommend < 7, but 20 is fun in its own way)
-#define SPEED     0.20  // Amount to increment RGB color by each cycle
+#define COLOR_ORDER GRB  // Try mixing up the letters (RGB, GBR, BRG, etc) for a whole new world of color combinations
+#define LEFT 1
+#define RIGHT 0
 
+CRGB leds[N_PIXELS];
 
 float
   greenOffset = 30,
   blueOffset = 150;
-
-byte
-  peak      = 0,      // Used for falling dot
-  dotCount  = 0,      // Frame counter for delaying dot-falling speed
-  volCount  = 0;      // Frame counter for storing past volume data
-
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -44,42 +32,16 @@ const uint32_t Green = strip.Color(0, 255, 0);
 const uint32_t Blue = strip.Color(0, 0, 255);
 const uint32_t White = strip.Color(255, 255, 255);
 const uint32_t Dark = strip.Color(0, 0, 0);
-uint8_t slug_size = 4;
 uint32_t colors[] = {Red, Yellow, Green, Blue, White};
 
-CRGB leds[N_PIXELS];
+// constants used here to set pin numbers:
+const int buttonPin = 3;     // the number of the pushbutton pin
 
-//Ripple variables
-int color;
-int center = 0;
-int step = -1;
-int maxSteps = 8;
-float fadeRate = 0.80;
-int diff;
- 
-//background color
-uint32_t currentBg = random(256);
-uint32_t nextBg = currentBg;
+// Variables will change:
 
-/*void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}*/
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
@@ -96,9 +58,11 @@ uint32_t Wheel(byte WheelPos) {
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
+void colorWipe(uint32_t c, uint8_t wait, uint8_t dir) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
+      auto index = i;
+      if (dir == LEFT) index = strip.numPixels() - i - 1;
+      strip.setPixelColor(index, c);
       strip.show();
       delay(wait);
   }
@@ -129,6 +93,30 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
+void vu(int height) {
+   // Color pixels based on rainbow gradient
+   for(auto i=0; i<N_PIXELS_HALF; i++) {
+    if(i >= height) {              
+      strip.setPixelColor(N_PIXELS_HALF-i-1,   0,   0, 0);
+      strip.setPixelColor(N_PIXELS_HALF+i,   0,   0, 0);
+    }
+    else {
+      uint32_t color = Wheel(map(i,0,N_PIXELS_HALF-1,(int)greenOffset, (int)blueOffset));
+      strip.setPixelColor(N_PIXELS_HALF-i-1,color);
+      strip.setPixelColor(N_PIXELS_HALF+i,color);
+    }
+   }
+   strip.show();
+}
+
+void swipe() {
+  for (auto i = 0; i < sizeof(colors)/sizeof(*colors); i++) {
+    colorWipe(colors[i], 25, RIGHT);
+  }
+  for (auto i = 0; i < sizeof(colors)/sizeof(*colors); i++) {
+    colorWipe(colors[i], 25, LEFT);
+  }
+}
 
 const int sampleWindow = 100; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
@@ -137,6 +125,9 @@ void setup()
 {
    Serial.begin(115200);
    delay( 2000 ); // power-up safety delay
+   pinMode(buttonPin, INPUT);  
+   //initialize the buttonPin as output
+   digitalWrite(buttonPin, HIGH); 
    FastLED.addLeds<WS2812B, PIN, COLOR_ORDER>(leds, N_PIXELS).setCorrection( TypicalLEDStrip );
    FastLED.setBrightness(  BRIGHTNESS );
    LEDS.addLeds<LED_TYPE, PIN, COLOR_ORDER>(leds, N_PIXELS); 
@@ -176,35 +167,49 @@ void loop()
 
    auto max = 3;
    auto min = 0;
-
    auto height = (int) ( N_PIXELS / (max - min) * volts);
 
-   // Color pixels based on rainbow gradient
-   /*for(auto i=0; i<N_PIXELS_HALF; i++) {
-    if(i >= height) {              
-      strip.setPixelColor(N_PIXELS_HALF-i-1,   0,   0, 0);
-      strip.setPixelColor(N_PIXELS_HALF+i,   0,   0, 0);
-    }
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      if(buttonPushCounter>=4) buttonPushCounter=1;
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonPushCounter);
+      for(uint16_t i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, Dark);
+      strip.show(); // Initialize all pixels to 'off'
+    } 
     else {
-      uint32_t color = Wheel(map(i,0,N_PIXELS_HALF-1,(int)greenOffset, (int)blueOffset));
-      strip.setPixelColor(N_PIXELS_HALF-i-1,color);
-      strip.setPixelColor(N_PIXELS_HALF+i,color);
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off"); 
     }
-   }
-   strip.show();*/
+  }
+  // save the current state as the last state, 
+  // for next time through the loop
+  
+  lastButtonState = buttonState;
 
-  /*uint16_t i, j;
-  for(j=0; j<256; j++) { // 1 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      //strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-      strip.setPixelColor(i, Wheel(j));
-    }
-    strip.show();
-    delay(5);
-  }*/
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  rainbow(20);
-  rainbowCycle(20);
+  switch (buttonPushCounter) {  
+  case 1:
+  {
+    vu(height);
+    break;
+  }
+  case 2:
+  {
+    swipe();
+    break;
+  }
+  default:
+  {
+    rainbow(10);
+  }
+  }
 }
